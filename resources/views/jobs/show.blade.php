015d47d2e6ef32212deb60f7c1782d3acc83139a
Kleiton-CF <kleiton3freitas@gmail.com>
2024-05-24 10:59:13 -0300

    Form 3 - Day 18

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,9 +2,15 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+
     <p>
-        This Job pays {{ $job['salary'] }}, per year.
+        This Job pays {{ $job->salary }}, per year.
+    </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
    
 </x-layout>
